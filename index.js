4f5af2fdd2dd63a3ec7c090dd968592ddf080b79
Artur Arutyunov <aarutyunov852005@gmail.com>
2022-11-28 03:29:24 +0500

    asdff

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 
 app.post("/api/auth", async(req, res)=>{
     let a = req.body;
-    res.status(200).json({a})
+    res.status(200).json({...a})
 })
 
 app.listen(process.env.PORT || 4444, (err) => {
