9c386c3837bd757fecfadd9ec35eaf85636dcff4
Artur Arutyunov <aarutyunov852005@gmail.com>
2022-11-28 02:17:00 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express-serve-static-core";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  res.status(200).json({ message: "OK" });
+});
+
+app.listen(process.env.PORT || 4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`Server started on port:${PORT}`);
+});
