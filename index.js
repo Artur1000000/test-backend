a99c6746e3cba804babb8a91dd36f04c99f00791
Artur Arutyunov <aarutyunov852005@gmail.com>
2022-11-28 02:54:38 +0500

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.post("/", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({ message: "OK" });
 });
 
@@ -15,5 +15,5 @@ app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err);
   }
-  console.log(`Server started on port:${PORT}`);
+  console.log(`Server started on port:${process.env.PORT? process.env.PORT: 4444}`);
 });
